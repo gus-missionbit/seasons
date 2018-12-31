85777d38d29bf4fb507a242abbe7dc6513185cfe
Gustavo Rodriguez Salas <gstvo.rodriguez@gmail.com>
2018-12-31 14:27:10 -0800

    ready for meal list

--- season.js
+++ season.js
@@ -1,10 +1,11 @@
 
 //const seasonRoot = document.getElementById('fruits')
 const fruitsDiv = document.getElementById('fruits')
-const category1div = document.getElementById('category1')
-const category2div = document.getElementById('category2')
-const category3div = document.getElementById('category3')
-const category4div = document.getElementById('category4')
+const receipt = document.getElementById('createReceipt')
+// const category1div = document.getElementById('category1')
+// const category2div = document.getElementById('category2')
+// const category3div = document.getElementById('category3')
+// const category4div = document.getElementById('category4')
 
 
 // category1div.style.color = "red"
@@ -160,10 +161,10 @@ let waterScore = 0
 
 function advance() {
 	if (currentQuestion===3){
-		category1div.textContent = ""
-		category2div.textContent = ""
-		category3div.textContent = ""
-		category4div.textContent = ""
+		// category1div.textContent = ""
+		// category2div.textContent = ""
+		// category3div.textContent = ""
+		// category4div.textContent = ""
 
 		fruitsDiv.textContent = "All done"
 
@@ -173,31 +174,97 @@ function advance() {
 			" water: " + waterScore;
 
 		alert(score);
-	} else {
+	} else if(currentQuestion===0){
 		currentQuestion += 1
-		fruitsDiv.textContent = seasonalPick[currentQuestion-1]
-		category1div.textContent = fruit[currentQuestion-1][0]
-		category2div.textContent = fruit[currentQuestion-1][1]
-		category3div.textContent = fruit[currentQuestion-1][2]
-		category4div.textContent = fruit[currentQuestion-1][3]
+
+		var toAdd = document.createDocumentFragment();
+		document.getElementById("demo").innerHTML = fruit[currentQuestion-1].length;
+		for(var i=0; i < fruit[currentQuestion-1].length; i++){
+   			var newDiv = document.createElement('div');
+   			newDiv.id = 'f'+i;
+   			newDiv.className = 'listFruits';
+   			newDiv.textContent = fruit[currentQuestion-1][i];
+   			toAdd.appendChild(newDiv);
+		}
+    	document.getElementById('fruits').appendChild(toAdd);
+    }else if(currentQuestion===1){
+    	currentQuestion += 1
+    	
+    	fruitsDiv.textContent = ""
+
+		var toAdd = document.createDocumentFragment();
+		
+		var newDiv = document.createElement('div');
+   		newDiv.id = 'breakfast';
+   		newDiv.className = 'choosenFruitsBreakFast';
+   		newDiv.textContent = "Todays breakfast meal:";
+   		toAdd.appendChild(newDiv);
+
+   		var newDiv = document.createElement('div');
+   		newDiv.id = 'lunch';
+   		newDiv.className = 'choosenFruitsLunch';
+   		newDiv.textContent = "With this for lunch:";
+   		toAdd.appendChild(newDiv);
+
+   		var newDiv = document.createElement('div');
+   		newDiv.id = 'dinner';
+   		newDiv.className = 'choosenFruitsDinner';
+   		newDiv.textContent = "Wrapping up the day with dinner:";
+   		toAdd.appendChild(newDiv);
+
+		for(var i=0; i < choosenFruits.length; i++){
+   			var newDiv = document.createElement('div');
+   			newDiv.id = 'f'+i;
+   			newDiv.className = 'choosenFruits';
+   			newDiv.textContent = choosenFruits[i];
+   			toAdd.appendChild(newDiv);
+		}
+    	document.getElementById('fruits').appendChild(toAdd);
+
+    	
     }
+
 }
-category1.onclick = function() {
-	fireScore += 1
-	advance()
-}
-category2.onclick = function() {
+var choosenFruits = []; 
+
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || target.innerText; 
+
+    document.getElementById("demo").innerHTML = e.target.id;
+    const choosen = document.getElementById(e.target.id);
+    choosenFruits.push(choosen.textContent);
+    document.getElementById("demo").innerHTML = choosenFruits.length;
+
+}, false);
+
+receipt.onclick = function() {
 	earthScore += 1
 	advance()
 }
-category3.onclick = function() {
-	airScore += 1
-	advance()
-}
-category4.onclick = function() {
-	waterScore += 1
-	advance()
-}
+// fruitsDiv.onclick = function() {
+
+// 	advance();
+
+// }
+// const category2div = document.getElementById('f'+1)
+// category1.onclick = function() {
+// 	fireScore += 1
+// 	advance()
+// }
+// category2.onclick = function() {
+// 	earthScore += 1
+// 	advance()
+// }
+// category3.onclick = function() {
+// 	airScore += 1
+// 	advance()
+// }
+// category4.onclick = function() {
+// 	waterScore += 1
+// 	advance()
+// }
 
 advance()
 
